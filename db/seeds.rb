58c0b2892d242aeeb6c6f5f30120f0f81c1590fd
Marian Serna <marianhalliday@gmail.com>
2017-10-18 11:15:40 -0400

    Generating seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: 'Cafe Nutibara', description: 'Fresh Colombian coffee from the central region', price: 16.50)
+
+Product.create(name: 'Chocolate Cruz', description: 'White chocolate from Cuzco', price: 10.03)
+
+Product.create(name: 'Cascara Tea', description: 'HErbal tea with peach flavour', price: 5.04)
