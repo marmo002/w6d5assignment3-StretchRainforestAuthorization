95eed12d5583d363e08a51246e74171e14f01cc9
Marian Serna <marianhalliday@gmail.com>
2017-10-18 13:40:08 -0400

    Applied method to display price in cents

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   validates :name, :description, :price, presence: true
 
-  # validates :price, numericality: { only_integer: true }
+  def price_in_dollars
+    price_in_dollars = price.to_f
+    sprintf("$%.2f", price_in_dollars)
+  end
 
 end
