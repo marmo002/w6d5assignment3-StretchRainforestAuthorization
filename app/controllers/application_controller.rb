b94bba54491728380b428a5b2edab457fa5d66fc
marmo002 <marmo_02@hotmail.com>
2017-10-21 12:47:57 -0400

    On ApplicationController, add method ensured_logged_in unless current_user is set redirect to log in session view. Add before_action ensured_logged_in to run before create, edit, update and destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def ensured_logged_in
+    unless current_user
+      flash[:alert] = "You must be logged in!"
+      redirect_to new_sessions_url
+    end
+  end
+
   private
 
   def current_user
